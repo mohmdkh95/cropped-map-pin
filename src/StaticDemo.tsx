dcd0c5517fab3789ccfaaef349b40320135cb364
mohmd95 <mohammed@getaway.pro>
2026-09-24 01:37:36 +0300

    my first commit

--- src/StaticDemo.tsx
+++ src/StaticDemo.tsx
@@ -10,7 +10,6 @@
  *
  * Run src/LiveMapRepro.tsx instead when you have a key.
  */
-//
 import React from 'react';
 import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import Pin, {PIN_H, PIN_W, TOP_MARGIN} from './Pin';
